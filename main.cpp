aada80c079a80e47a90fec9cffdda55b9de6bb9d
PietroFornari <100316910+PietroFornari@users.noreply.github.com>
2022-03-16 19:40:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main() {
         float a = 0, b = 0, c = 0, AreaTriangolo = 0, AreaQuadrato = 0, AreaRettangolo = 0, AreaTrapezio = 0;
-        cout << "Inserire qua sotto i valori (in cm) di a, b, c in questo ordine, separati tra di loro da uno spazio." << endl;
         cin >> a >> b >> c;
 
         AreaTriangolo = (a * b)/ 2;
@@ -11,12 +10,13 @@ int main() {
         AreaRettangolo = a* b;
         AreaTrapezio = ((a + b) * c)/ 2;
 
-        cout << "L'area del triangolo vale: " << AreaTriangolo << " cm" << endl;
-        cout << "L'area del quadrato vale: " << AreaQuadrato << " cm" << endl;
-        cout << "L'area del rettangolo vale: " << AreaRettangolo << " cm" << endl;
-        cout << "L'area del trapezio vale: " << AreaTrapezio << " cm" << endl;
+        cout << AreaTriangolo << endl;
+        cout << AreaQuadrato << endl;
+        cout << AreaRettangolo << endl;
+        cout << AreaTrapezio << endl;
 
         return 0;
+
 
 
 }
